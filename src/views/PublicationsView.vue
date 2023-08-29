7e873e8cbb4eaae1bf8b9b9406bc05c7f3e9a38d
hamed <amzhamed@gmail.com>
2023-08-29 08:13:24 -0700

    add publications

--- src/views/PublicationsView.vue
+++ src/views/PublicationsView.vue
@@ -1,11 +1,23 @@
 <script setup>
 const publications = [
   {
+    cite: "T. Phan, H. Alimohammadzadeh, H. Culbertson, and S. Ghandeharizadeh. An Evaluation of Three Distance Measurement Technologies for Flying Light Specks. 2023.",
+    pdf: "https://arxiv.org/pdf/2308.10115.pdf",
+  },
+  {
+    cite: "H. Alimohammadzadeh, R. Bernard, Y. Chen, T. Phan, P. Singh, S. Zhu, H. Culbertson, and S. Ghandeharizadeh. Dronevision: An Experimental 3D Testbed for Flying Light Specks. 2023.",
+    pdf: "https://arxiv.org/pdf/2308.10121.pdf",
+  },
+  {
+    cite: "H. Alimohammadzadeh, D. Mehraban, and S. Ghandeharizadeh. Modeling Illumination Data with Flying Light Specks. In Proceedings of the 14th Conference on ACM Multimedia Systems, Vancouver, Canada, June 2023.",
+    pdf: "https://dl.acm.org/doi/pdf/10.1145/3587819.3592544",
+  },
+  {
     cite: "S. Ghandeharizadeh. Display of 3D Illuminations using Flying Light Specks. In ACM Multimedia, Lisbon, Portugal, October 2022.",
     pdf: "https://arxiv.org/pdf/2207.08346.pdf",
   },
   {
-    cite: "S. Ghandeharizadeh and L. Garcia. Safety in the Emerging Holodeck Applications. In CHI 2022 Workshop on Novel Challenges of Safety, Security and Privacy in Extended Reality,  April 25, 2022.",
+    cite: "S. Ghandeharizadeh and L. Garcia. Safety in the Emerging Holodeck Applications. In CHI 2022 Workshop on Novel Challenges of Safety, Security and Privacy in Extended Reality, April 25, 2022.",
     pdf: "https://arxiv.org/pdf/2208.08398.pdf",
   },
   {
