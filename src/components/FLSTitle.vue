02a381cdcebc16f6e715f7cd262d163c3c1a7c45
arushi183 <120046339+arushi183@users.noreply.github.com>
2023-09-20 15:14:34 -0700

    Update FLSTitle.vue

--- src/components/FLSTitle.vue
+++ src/components/FLSTitle.vue
@@ -12,7 +12,7 @@ defineProps({
     <h1 class="text-5xl lg:text-8xl">
       <span
         class="font-black bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-pink-500"
-        >FLS</span
+        >DB</span
       >
       <span class="text-white">Lab</span>
     </h1>
