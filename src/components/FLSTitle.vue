87ef02ef58a9e00d719e0745616bf68020402726
arushi183 <120046339+arushi183@users.noreply.github.com>
2023-09-20 15:14:14 -0700

    Change logo color

--- src/components/FLSTitle.vue
+++ src/components/FLSTitle.vue
@@ -11,7 +11,7 @@ defineProps({
   <div>
     <h1 class="text-5xl lg:text-8xl">
       <span
-        class="font-black bg-clip-text text-transparent bg-gradient-to-r from-teal-500 via-sky-500 to-purple-500"
+        class="font-black bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-pink-500"
         >FLS</span
       >
       <span class="text-white">Lab</span>
