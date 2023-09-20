72b91eba8175e8449706c32681c61a68530426be
arushi183 <120046339+arushi183@users.noreply.github.com>
2023-09-20 15:15:10 -0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
 <template>
-  <div></div>
+  <div>
+    <h1 class="mb-8 text-4xl font-black text-white">Our Mission</h1>
+
+    <h3>
+      The DB Lab at University of Southern California is directed by Dr. Shahram Ghandeharizadeh and it conducts research on databases and storage managers.
+    </h3>
+  </div>
 </template>
